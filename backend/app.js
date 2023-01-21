cfb294885283f9d5ddda8661d7f5f0ec0579224f
aneeshb7 <aneesh.bulusu@gmail.com>
2023-01-21 15:29:46 -0800

    got chatbot up and running

--- backend/app.js
+++ backend/app.js
@@ -25,3 +25,24 @@ app.get('/', (req, res) => {
 app.get('/ping', (req, res) => {
     res.send('pong');
 });
+
+import { NlpManager } from "node-nlp";
+console.log("Starting Chatbot ...");
+const manager = new NlpManager({ languages: ["en"] });
+// Loading our saved model
+manager.load();
+
+// Loading a module readline, this will be able to take input from the terminal.
+import readline from "readline";
+var rl = readline.createInterface(process.stdin, process.stdout);
+console.log("Chatbot started!");
+rl.setPrompt("> ");
+rl.prompt();
+rl.on("line", async function (line) {
+  // Here Passing our input text to the manager to get response and display response answer.
+  const response = await manager.process("en", line);
+  console.log(response.answer);
+  rl.prompt();
+}).on("close", function () {
+  process.exit(0);
+});
