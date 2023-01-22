06de7e122e26e3a3885abcb681cb0bc30365a365
Aryan Gandhi <gandhi.aryan1310@gmail.com>
2023-01-21 16:22:49 -0800

    added some frontend functionality

--- frontend/screens/ClosestClinics.js
+++ frontend/screens/ClosestClinics.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {FlatList, StyleSheet, Text, View} from 'react-native';
+import controller from '../controller.js';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,22 +15,22 @@ const styles = StyleSheet.create({
 });
 
 export default function ClosestClinics() {
+
+  const [locations, setLocations] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const locations = await controller.getAllLocations();
+      setLocations(locations);
+    }
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
       <FlatList
-        data={[
-          {key: 'clinic #1'},
-          {key: 'clinic #2'},
-          {key: 'clinic #3'},
-          {key: 'clinic #4'},
-          {key: 'clinic #5'},
-          {key: 'clinic #6'},
-          {key: 'clinic #7'},
-          {key: 'clinic #8'},
-          {key: 'clinic #9'},
-          {key: 'clinic #10'},
-        ]}
-        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        data={locations}
+        renderItem={({item}) => <Text style={styles.item}>{item.name}</Text>}
       />
     </View>
   );
