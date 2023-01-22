f40fa7e598872a1724a129a5cb2b780947b127b7
Aryan Gandhi <gandhi.aryan1310@gmail.com>
2023-01-21 16:37:04 -0800

    small fix

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ mongoUtil.connectToDatabase( function( err, client ) {
 
 const port = process.env.PORT || 8080;
 app.listen(port, () =>
-console.log(`Example app listening on port ${port}!`),
+  console.log(`Example app listening on port ${port}!`),
 );
 
 app.get('/', (req, res) => {
