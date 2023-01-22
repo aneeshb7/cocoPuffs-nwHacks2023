589c59a2420028ab57ee97946aa83b0b1a6e00d3
Aryan Gandhi <gandhi.aryan1310@gmail.com>
2023-01-21 17:37:51 -0800

    added geolocation ability

--- frontend/screens/ClosestClinics.js
+++ frontend/screens/ClosestClinics.js
@@ -2,6 +2,51 @@ import React, { useEffect, useState } from 'react';
 import {FlatList, Text, View, Button} from 'react-native';
 import styles from '../styles/StyleSheet';
 import controller from '../controller.js';
+import Geolocation from 'react-native-location';
+
+const getGPSLocation = async () => {
+  Geolocation.configure({
+         distanceFilter: 5.0,
+         desiredAccuracy: {
+                ios: 'best',
+                android: 'balancedPowerAccuracy',
+         },
+  });
+  //trycatch block to catch any async errors
+  try {
+    //check for permissions
+    const checkPerm = await Geolocation.checkPermission({
+        ios: 'whenInUse', 
+        android: {
+              detail: 'fine', 
+        },
+    });
+    //If not permission, ask for one
+    if (!checkPerm) {
+      const permGranted = await Geolocation.requestPermission({
+            ios: 'whenInUse',
+            android: {
+                detail: 'fine',
+                rationale: {
+                    title: 'We need to access your location',
+                    message: 'We use your location to show where you are on the map',
+                    buttonPositive: 'OK',
+                    buttonNegative: 'Cancel',
+                },
+              },
+          });
+      if (!permGranted) {
+          console.log('Permission not granted');
+          throw Error('Permission not granted﻿');
+      }
+    }
+    //Get the location information only with required permissions
+    const position = await Geolocation.getLatestLocation({timeout: 70000});
+    alert(JSON.stringify(position));
+  } catch (error) {
+    console.log('ERROR', error);
+  }
+};
 
 
 export default function ClosestClinics() {
@@ -14,6 +59,7 @@ export default function ClosestClinics() {
       setLocations(locations);
     }
     fetchData();
+    getGPSLocation();
   }, []);
 
   return (
